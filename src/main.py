dcaa80f8f099ffa7c52c0cda8e14ed5808c8109f
LanskovNV <leins275@gmail.com>
2021-08-27 22:44:09 +0300

    Updated

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from file_operations.move_all_to_root import move_all_to_root
-
+from file_operations.remove_empty_subdirs import remove_empty_subdirs
 
 if __name__ == '__main__':
     base_path = '/Users/leins275/Data/Storage/Книги/Books'
+    remove_empty_subdirs(base_path)
     # move_all_to_root(base_path)
